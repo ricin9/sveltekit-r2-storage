8197bef91b0df6c3f8dd87e4b3e543cdeaed7ce2
ricin9 <ricin9@outlook.com>
2024-08-27 15:26:15 +0100

    changed static private envars to dynamic

--- src/lib/server/S3.ts
+++ src/lib/server/S3.ts
@@ -1,15 +1,11 @@
-import {
-  R2_ACCESS_KEY_ID,
-  R2_ENDPOINT,
-  R2_SECRET_ACCESS_KEY,
-} from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const S3 = new S3Client({
   region: "auto",
-  endpoint: R2_ENDPOINT,
+  endpoint: env.R2_ENDPOINT,
   credentials: {
-    accessKeyId: R2_ACCESS_KEY_ID,
-    secretAccessKey: R2_SECRET_ACCESS_KEY,
+    accessKeyId: env.R2_ACCESS_KEY_ID,
+    secretAccessKey: env.R2_SECRET_ACCESS_KEY,
   },
 });
